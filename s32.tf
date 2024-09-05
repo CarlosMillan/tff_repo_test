7bc233f153ea6accc1d8b5bc3ceac900951b9a8e
Carlos Millán <charlyomo@gmail.com>
2024-09-05 09:18:20 -0600

    Create s32.tf

---
+++ s32.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "mi-bucket-deterraform-cloud-local-1321564"
+}
+
+resource "aws_s3_bucket" "my_bucket2" {
+  bucket = "mi-bucket-deterraform-cloud-local-564565"
+}
+
+resource "aws_s3_bucket" "my_bucket3" {
+  bucket = "mi-bucket-deterraform-cloud-local-13214678678564"
+}
